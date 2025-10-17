10ef5364d9d570e70dcaf190e7357f33f321d7da
Luiz D. M. Mainart <luizdmmainart@gmail.com>
2025-10-17 14:20:09 -0300

    feat(zendesk-sell): Fix index.ts

--- packages/pieces/community/zendesk-sell/src/index.ts
+++ packages/pieces/community/zendesk-sell/src/index.ts
@@ -1,20 +1,26 @@
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework';
+import { createContact } from './lib/actions/create-contact';
+import { createDeal } from './lib/actions/create-deal';
+import { createLead } from './lib/actions/create-lead';
+import { createNote } from './lib/actions/create-note';
+import { createTask } from './lib/actions/create-task';
+import { updateContact } from './lib/actions/update-contact';
+import { updateDeal } from './lib/actions/update-deal';
 
-    import { createPiece, PieceAuth } from "@activepieces/pieces-framework";
-import { createContact } from "./lib/actions/create-contact";
-import { createLead } from "./lib/actions/create-lead";
-import { createDeal } from "./lib/actions/create-deal";
-import { updateContact } from "./lib/actions/update-contact";
-import { updateDeal } from "./lib/actions/update-deal";
-import { createNote } from "./lib/actions/create-note";
-import { createTask } from "./lib/actions/create-task";
-
-    export const zendeskSell = createPiece({
-      displayName: "Zendesk-sell",
-      auth: PieceAuth.None(),
-      minimumSupportedRelease: '0.36.1',
-      logoUrl: "https://cdn.activepieces.com/pieces/zendesk-sell.png",
-      authors: [],
-      actions: [createContact, createLead, createDeal, updateContact, updateDeal, createNote, createTask],
-      triggers: [],
-    });
-    
+export const zendeskSell = createPiece({
+  displayName: 'Zendesk Sell',
+  auth: PieceAuth.None(),
+  minimumSupportedRelease: '0.36.1',
+  logoUrl: 'https://cdn.activepieces.com/pieces/zendesk-sell.png',
+  authors: ['anamainart', 'LuizDMM'],
+  actions: [
+    createContact,
+    createLead,
+    createDeal,
+    updateContact,
+    updateDeal,
+    createNote,
+    createTask,
+  ],
+  triggers: [],
+});
